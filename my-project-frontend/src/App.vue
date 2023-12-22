880bfc41df93772f445d5ebf6a1ca280c804fe42
HoshiSrar <568541648@qq.com>
2023-12-22 09:48:15 +0800

    前端初始项目创建

--- my-project-frontend/src/App.vue
+++ my-project-frontend/src/App.vue
@@ -1,18 +1,26 @@
 <template>
-  <MyLogin></MyLogin>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import MyLogin from "./components/login"
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    MyLogin
+    HelloWorld
   }
 }
 </script>
 
 <style>
-
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
