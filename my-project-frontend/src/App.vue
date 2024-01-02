1c92946234ae9378c296f6b257d97c211aa34e06
HoshiSrar <568541648@qq.com>
2024-01-02 08:29:19 +0800

    重置密码

--- my-project-frontend/src/App.vue
+++ my-project-frontend/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { useDark, useToggle } from '@vueuse/core'
-
+import {reactive ,ref } from 'vue'
 useDark({
   selector: 'html',
   attribute: 'class',
@@ -11,7 +11,12 @@ useDark({
 useDark({
   onChanged(dark) { useToggle(dark) }
 })
-
+const from = reactive({
+  username:'djt',
+  password:'123',
+  niki:'nike'
+})
+const b = ref('密码')
 </script>
 
 <template>
